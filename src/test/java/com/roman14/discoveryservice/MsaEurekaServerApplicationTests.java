225c8716b2a80329936165f9ff424f0c8f603b39
= <roman14@naver.com>
2022-12-04 23:24:16 +0900

    Initial commit

---
+++ src/test/java/com/roman14/discoveryservice/MsaEurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.roman14.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaEurekaServerApplicationTests
+{
+
+  @Test
+  void contextLoads()
+  {
+  }
+
+}
